045d65570a931e3ab621fb66e8e50cf36b7ab819
mgreen1092 <morgan.green0209@gmail.com>
2022-12-30 10:00:36 -0500

    states added to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [user, setUser] = useState('')
+  const [goal, setGoal] = useState({})
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loggingIn, setLoggingIn] = useState('')
   return (
     <div className="App">
       <h1>GOAL MASTER</h1>
