0ba0ca1bf80055a43c09fd5a4bb31e473bc4eec5
mgreen1092 <morgan.green0209@gmail.com>
2022-12-30 10:12:53 -0500

    setup main page

---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../MainPage/MainPage.css'
+import axios from 'axios'
+
+export default function MainPage ({}) {
+    // const updatedUser = await axios.get()
+    return (
+        <div>
+
+        </div>
+    )
+}
