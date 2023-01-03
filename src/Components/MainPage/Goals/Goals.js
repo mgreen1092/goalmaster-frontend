e3e48da44b3d1448375e6b508abed7ed5172a956
mgreen1092 <morgan.green0209@gmail.com>
2023-01-03 10:19:39 -0500

    adding users to backend error in userçontext

--- src/Components/MainPage/Goals/Goals.js
+++ src/Components/MainPage/Goals/Goals.js
@@ -1,10 +1,30 @@
 import React from "react";
 import '../Goals/Goals.css'
+// import axios from 'axios'
 
-export default function Goals () {
+export default function Goals ({goals, setGoals}) {
+    // const getGoals = async () => {
+    //     axios.get(`https://goalmaster.herokuapp.com/api/users/${user.email}/`).then(response => {
+    //         setGoals(response.data.goals)
+    //         console.log(response.data.goals)
+    // })}
+    // const handleAddGoals = async () => {
+    //     const newGoal = await axios.post('https://goalmaster.herokuapp.com/api/goals', {
+    //         goal: 'My new goal:',
+    //         description: '',
+    //         goalvalue: null,
+    //         occurence: '',
+    //         tracker: []
+    //     })
+    //     await axios.put(`https://goalmaster.herokuapp.com/api/users/${user.email}/add`, {
+    //         _id: newGoal.data._id
+    //     })
+    //     const updatedUser = await axios.get(`https://spotifly-backend-ga.herokuapp.com/api/users/${user.email}`)
+    //     // setUser(updatedUser.data)
+    //     }
     return (
         <div>
-
+            
         </div>
     )
 }
